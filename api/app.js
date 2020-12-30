c2417f6d6783eecd7961974d6c871ac28f55d5e0
ABourneron <antoine.bourneron@gmail.com>
2020-12-30 22:22:30 +0100

    Passage de la connexion de l'API a Mongose

--- api/app.js
+++ api/app.js
@@ -5,18 +5,20 @@ const app = express()
 const port = 3000
 var mongo = require('mongodb')
 
-var MongoClient = require('mongodb').MongoClient;
-var url = process.env.URL_MONGO;
-//var url = "mongodb://mongo:27080/";
+//var url = process.env.URL_MONGO;
+var url = "mongodb://mongo:27017/mobyWeb";
 console.log(url);
-
-var dbName = "mobyWeb";
-var collectionName = "mobs";
 
 app.use(express.static('static'));
 app.use(express.json());
 
 app.use(cors());
+
+var mongoose = require('mongoose');
+mongoose.connect(url, function(err) {
+  console.log("Database connection ok");
+  if (err) { throw err; }
+});
 
 //Page d'accueil
 app.get('/', async (req, res) => {
@@ -28,32 +30,44 @@ app.get('/', async (req, res) => {
   });
 });
 
+//Schéma
+var mobsSchema = new mongoose.Schema({
+  name: String,
+  status: String,
+  desc: String,
+  price: String
+});
+
+var mobsModel = new mongoose.model('Mobs', mobsSchema)
+var maMobs = new mobsModel({name:"Peugeot 104"});
+maMobs.desc = "Essai BDD";
+
+maMobs.save(function (err) {
+  if (err) { throw err; }
+  console.log("Mobs ajoutée");
+});
+
 //Récuperation de toutes les mobs
 app.get('/api/mobs', function (req, res) {
-  MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db(dbName);
-    dbo.collection(collectionName).find({}).toArray(function (err, result) {
-      if (err) throw err;
-      res.send(result);
-      db.close();
-    });
+  mobsModel.find(null, function (err, comms) {
+    if (err) { throw err; }
+    res.send(comms);
   });
-})
+});
 
 //Récuperation d'une mobs en particulier
 app.get('/api/mob/:id', function (req, res) {
   var varID = mongo.ObjectID(req.params.id)
-  MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db(dbName);
-    dbo.collection(collectionName).find({ _id: varID }).toArray(function (err, result) {
-      if (err) throw err;
-      res.send(result);
-      db.close();
-    });
+
+  var query = mobsModel.find(null);
+  query.where("_id", varID);
+
+  query.exec(function (err, comms) {
+    if (err) { throw err; }
+    res.send(comms);
   });
-})
+});
+
 /*
 //Ajout d'une personne
 app.post('/api/personnes', function (req, res) {
@@ -61,22 +75,25 @@ app.post('/api/personnes', function (req, res) {
   MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
     if (err) throw err;
     var dbo = db.db("test");
-
     dbo.collection("personne").insertOne(req.body, function (err) {
       if (err) throw err;
       res.send(req.body);
       db.close();
     });
   });
-})
 
+
+  maMobs.save(function (err) {
+  if (err) { throw err; }
+  console.log("Mobs ajoutée");
+});
+})
 //Suppresion d'une personne
 app.delete('/api/personnes/:id', function (req, res) {
   var varID = mongo.ObjectID(req.params.id)
   MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
     if (err) throw err;
     var dbo = db.db("test");
-
     dbo.collection("personne").deleteOne({ _id: varID }, function (err) {
       if (err) throw err;
       console.log("1 delete");
@@ -84,7 +101,6 @@ app.delete('/api/personnes/:id', function (req, res) {
     });
   });
 })
-
 //MAJ d'une personne en particulier
 app.put('/api/personnes/:id', function (req, res) {
   var varID = mongo.ObjectID(req.params.id)
@@ -99,7 +115,6 @@ app.put('/api/personnes/:id', function (req, res) {
         sexe: req.body.sexe
       }
     };
-
     dbo.collection("personne").updateOne({ _id: varID }, myobj, (function (err, result) {
       if (err) throw err;
       res.send(result);
